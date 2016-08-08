91aaa914ca7b4a6c4139bebf764925b9d3b3a63f
Wojciech Soltys <wojciech_soltys@epam.com>
2016-08-08 10:46:31 +0200

    s1 - missing files

--- src/main/java/com/epam/katowice/MovieRentalController.java
+++ src/main/java/com/epam/katowice/MovieRentalController.java
@@ -1,8 +1,20 @@
 package com.epam.katowice;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by Wojciech_Soltys on 08.08.2016.
  */
+
+@RestController
 public class MovieRentalController {
+
+    @RequestMapping("/")
+    public String greeting() {
+        return "Welcome in Movies Rental!";
+    }
 
 }
